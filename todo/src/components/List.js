7e3a20f6d47f91c25795892d7fbc9193b8a5aa99
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-03 16:36:40 -0300

    create list item

--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -1,40 +1,12 @@
 import React from "react";
 
-import Card from "./Card";
-
-function DoneImg(props) {
-  if (props.done) {
-    return <img alt="done" src="./assets/on.png"></img>;
-  }
-
-  return <img alt="undone" src="./assets/off.png"></img>;
-}
+import ListItem from "./ListItem";
 
 function List(props) {
   return (
     <ul>
       {props.items.map(item => (
-        <li key={item.id}>
-          <Card className={item.done ? "done item" : "item"}>
-            {item.text}
-            <div>
-              <button
-                onClick={() => {
-                  props.onDone(item);
-                }}
-              >
-                <DoneImg done={item.done}></DoneImg>
-              </button>
-              <button
-                onClick={() => {
-                  props.onItemDeleted(item);
-                }}
-              >
-                <img alt="delete" src="./assets/trash.png"></img>
-              </button>
-            </div>
-          </Card>
-        </li>
+        <ListItem key={item.id} item={item} onDone={props.onDone} onItemDeleted={props.onItemDeleted}></ListItem>
       ))}
     </ul>
   );
