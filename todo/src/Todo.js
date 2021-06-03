c74dc1d187a3e4645cc97d62aea191074c11bd30
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-03 15:13:39 -0300

    action add item

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Todo.css";
 
 function Todo() {
+  const [text, setText] = useState("");
+  const [items, setItems] = useState([]);
+
+  function handleChange(event) {
+    let t = event.target.value;
+    setText(t);
+  }
+
+  function addItem(event) {
+    event.preventDefault();
+
+    if (text) {
+      setItems([...items, text]);
+      setText("");
+    }
+  }
+
   return (
     <div className="container">
       <h1>Todo</h1>
       <form>
-        <input type="text"></input>
-        <button>Add</button>
+        <input onChange={handleChange} type="text" value={text}></input>
+        <button onClick={addItem}>Add</button>
       </form>
       <ul>
-        <li>Item</li>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
       </ul>
     </div>
   );
