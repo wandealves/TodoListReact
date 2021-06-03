fb473502d4173cccf3a2e2e8565c4c63f70be42b
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-03 15:30:20 -0300

    add component form

--- todo/src/Todo.js
+++ todo/src/Todo.js
@@ -1,34 +1,21 @@
 import React, { useState } from "react";
 
+import TodoForm from './TodoForm';
+import List from "./List";
+
 import "./Todo.css";
 
-import List from './List';
-
 function Todo() {
-  const [text, setText] = useState("");
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    let t = event.target.value;
-    setText(t);
-  }
-
-  function addItem(event) {
-    event.preventDefault();
-
-    if (text) {
-      setItems([...items, text]);
-      setText("");
-    }
+  function onAddItem(item) {
+    setItems([...items, item]);
   }
 
   return (
     <div className="container">
       <h1>Todo</h1>
-      <form>
-        <input onChange={handleChange} type="text" value={text}></input>
-        <button onClick={addItem}>Add</button>
-      </form>
+      <TodoForm onAddItem={onAddItem}></TodoForm>
       <List items={items}></List>
     </div>
   );
