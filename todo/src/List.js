38ff3010c8d89912da5d6932d3b4ef06df3ac94a
Wanderson Alves Rodrigues <wanderson@pop-os.localdomain>
2021-06-03 16:11:53 -0300

    action done item

--- todo/src/List.js
+++ todo/src/List.js
@@ -1,16 +1,25 @@
 import React from "react";
 
+function DoneImg(props){
+  if(props.done){
+    return (<img alt="done" src="./assets/on.png"></img>);
+  }
+
+  return (<img alt="undone" src="./assets/off.png"></img>);
+}
+
 function List(props) {
   return (
     <ul>
       {props.items.map(item => (
-        <li key={item.id}>
+        <li key={item.id} className={item.done ? "done" : ""}>
           {item.text}
+          <button onClick={() => {props.onDone(item)}}><DoneImg done={item.done}></DoneImg></button>
           <button
             onClick={() => {
               props.onItemDeleted(item);
-            }}>
-
+            }}
+          >
             <img alt="delete" src="./assets/trash.png"></img>
           </button>
         </li>
